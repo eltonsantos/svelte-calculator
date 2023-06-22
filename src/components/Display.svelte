f68e7149ab2fa7fb95117a5c6b83ec8c24703862
Elton Santos <eltonaxl@hotmail.com>
2023-06-21 23:39:20 -0300

    Fix code

--- src/components/Display.svelte
+++ src/components/Display.svelte
@@ -25,24 +25,31 @@
   .s-14 {
     font-size: 1.5rem;
   }
+
   .s-15 {
     font-size: 1.4rem;
   }
+  
   .s-16 {
     font-size: 1.3rem;
   }
+  
   .s-17 {
     font-size: 1.25rem;
   }
+  
   .s-18 {
     font-size: 1.19rem;
   }
+  
   .s-19 {
     font-size: 1.13rem;
   }
+  
   .s-20 {
     font-size: 1.06rem;
   }
+  
   .pequena {
     font-size: 0.9rem;
   }
